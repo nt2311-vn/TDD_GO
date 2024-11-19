77dae7701264ecfad0c4021216ffcc6ad2ec7953
nt2311-vn <nguyentoan231196@gmail.com>
2024-11-19 11:41:39 +0700

    test two cases of sum of a slices

--- arrayandslices/sum.go
+++ arrayandslices/sum.go
@@ -1,6 +1,6 @@
 package arrayandslices
 
-func Sum(numbers [5]int) (res int) {
+func Sum(numbers []int) (res int) {
 	for _, num := range numbers {
 		res += num
 	}
--- arrayandslices/sum_test.go
+++ arrayandslices/sum_test.go
@@ -3,17 +3,26 @@ package arrayandslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("simple array into sum", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
 
 		assertCorrectResult(t, numbers, got, want)
 	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 50}
+
+		got := Sum(numbers)
+		want := 56
+
+		assertCorrectResult(t, numbers, got, want)
+	})
 }
 
-func assertCorrectResult(t testing.TB, numbers [5]int, got, want int) {
+func assertCorrectResult(t testing.TB, numbers []int, got, want int) {
 	t.Helper()
 
 	if got != want {
