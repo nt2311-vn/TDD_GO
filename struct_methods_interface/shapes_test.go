97446a33586ef14d32b82e3974ebcfbaa7f8e7e3
nt2311-vn <nguyentoan231196@gmail.com>
2024-11-19 15:37:08 +0700

    methods to type of struct rectangle or circle and write method to pass the test case

--- struct_methods_interface/shapes_test.go
+++ struct_methods_interface/shapes_test.go
@@ -3,21 +3,34 @@ package structmethodsinterface
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	assertCorrectResult(t, got, want)
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
 
-	assertCorrectResult(t, got, want)
+		assertCorrectResult(t, got, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+
+		got := circle.Area()
+		want := 314.1592653589793
+
+		assertCorrectResult(t, got, want)
+	})
 }
 
 func assertCorrectResult(t testing.TB, got, want float64) {
 	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+		t.Errorf("got %g want %g", got, want)
 	}
 }
