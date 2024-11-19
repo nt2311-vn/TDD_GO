9a5f38ad7b24b2c261a57b291976a3b51f652ab4
nt2311-vn <nguyentoan231196@gmail.com>
2024-11-19 10:06:25 +0700

    sub test in test function with t.Run and fix function to satisfy test want

--- hell_world/hello.go
+++ hell_world/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishPrefixHello = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishPrefixHello + name
 }
 
 func main() {
