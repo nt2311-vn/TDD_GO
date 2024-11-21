ea2103cc73db44a019fa4581fe35538a997f204e
nt2311-vn <nguyentoan231196@gmail.com>
2024-11-21 10:02:16 +0700

    write enough code to pass the test

--- concurency/checkwebsites.go
+++ concurency/checkwebsites.go
@@ -3,4 +3,15 @@ package concurency
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, webs []string) map[string]bool {
+	rs := make(map[string]bool)
+
+	for _, web := range webs {
+		if wc(web) {
+			rs[web] = true
+		} else {
+			rs[web] = false
+		}
+	}
+
+	return rs
 }
