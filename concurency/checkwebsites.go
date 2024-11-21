b98257837e1c73042f0f108cdfda2d681ce48719
nt2311-vn <nguyentoan231196@gmail.com>
2024-11-21 14:45:25 +0700

    to use go channel to pass the race condition

--- concurency/checkwebsites.go
+++ concurency/checkwebsites.go
@@ -2,11 +2,24 @@ package concurency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, webs []string) map[string]bool {
 	rs := make(map[string]bool)
+	rsChanel := make(chan result)
 
 	for _, web := range webs {
-		rs[web] = wc(web)
+		go func(u string) {
+			rsChanel <- result{u, wc(u)}
+		}(web)
+	}
+
+	for i := 0; i < len(webs); i++ {
+		r := <-rsChanel
+		rs[r.string] = r.bool
 	}
 
 	return rs
