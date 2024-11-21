6fdf34efe6d168e979244a7966d328c160a098d3
nt2311-vn <nguyentoan231196@gmail.com>
2024-11-21 16:29:12 +0700

    write second case to return error when server did not respond within 10second, rewrite fucntion

--- select/racer.go
+++ select/racer.go
@@ -1,16 +1,21 @@
 package select_mod
 
 import (
+	"errors"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) string {
+var ErrServerNotResp = errors.New("server not respond")
+
+func Racer(a, b string) (string, error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", ErrServerNotResp
 	}
 }
 
--- select/racer_test.go
+++ select/racer_test.go
@@ -8,21 +8,36 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayServer(2 * time.Second)
-	fastServer := makeDelayServer(0 * time.Second)
+	t.Run("compares speeds of servers, returning the url of fastest one", func(t *testing.T) {
+		slowServer := makeDelayServer(2 * time.Second)
+		fastServer := makeDelayServer(0 * time.Second)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q but want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q but want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayServer(11 * time.Second)
+		serverB := makeDelayServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 }
 
 func makeDelayServer(delay time.Duration) *httptest.Server {
