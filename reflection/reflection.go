330e533596c5405efb994527aacb294c1d1e3e70
nt2311-vn <nguyentoan231196@gmail.com>
2024-11-22 13:32:14 +0700

    update the test and walk function for check the element inside the slice is correct as provided and expected

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,5 +1,10 @@
 package reflection
 
+import "reflect"
+
 func walk(x interface{}, fn func(string)) {
-	fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+
+	fn(field.String())
 }
