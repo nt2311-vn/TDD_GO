7c2796ddcce0732637fa0b8689c1b025090df661
nt2311-vn <nguyentoan231196@gmail.com>
2024-11-25 10:11:12 +0700

    init module context and write test case for it

---
+++ context/context.go
@@ -0,0 +1,32 @@
+package context_mod
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
