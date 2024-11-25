79618f16b6b347b9923dc7e0955f741ae2d93a4f
nt2311-vn <nguyentoan231196@gmail.com>
2024-11-25 15:10:12 +0700

    write numerous addtional test case up to 3999 and ensure it passes with green

--- property_base_test/numeral_test.go
+++ property_base_test/numeral_test.go
@@ -22,6 +22,17 @@ func TestRomanNumerals(t *testing.T) {
 		{"40 gets converted to XL", 40, "XL"},
 		{"47 gets converted to XLVII", 47, "XLVII"},
 		{"50 gets converted to L", 50, "L"},
+		{"100 gets converted to C", 100, "C"},
+		{"90 gets converted to XC", 90, "XC"},
+		{"400 gets converted to CD", 400, "CD"},
+		{"500 gets converted to D", 500, "D"},
+		{"900 gets converted to CM", 900, "CM"},
+		{"1000 gets converted to M", 1000, "M"},
+		{"1984 gets converted to MCMLXXXIV", 1984, "MCMLXXXIV"},
+		{"3999 gets converted to MMMCMXCIX", 3999, "MMMCMXCIX"},
+		{"2014 gets converted to MMXIV", 2014, "MMXIV"},
+		{"1006 gets converted to MVI", 1006, "MVI"},
+		{"798 gets converted to DCCXCVIII", 798, "DCCXCVIII"},
 	}
 
 	for _, test := range cases {
